ebd637721552a1f317527da87ffa7331f2c79733
BorpZio <parusolanki117@gmail.com>
2024-12-03 21:23:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BorpZio/LimastoTV /LimastoTV
+  git clone https://github.com/BorpZio/LimastoTV/LimastoTV
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LimastoTV
